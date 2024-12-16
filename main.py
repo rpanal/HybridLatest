9800eb211db26e023cfcaf4e00b3515548a0008a
rpanal <restiep@gmail.com>
2024-12-17 07:17:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit as st
-from pages import genealogy,logindb,login
+from pages import genealogy,logindb
 
 def main():
 
