a521e0a00ad8108a629e29c70b058d8d1fb32aeb
rpanal <restiep@gmail.com>
2024-12-17 06:38:29 +0800

    Add files via upload

--- Pages/genealogy.py
+++ Pages/genealogy.py
@@ -6,18 +6,30 @@ import pyodbc
 conn = sqlite3.connect('./data/hybrid.db')
 
 current_user = ""
-user_name = st.text_input("Username")
-#conpy = pyodbc.connect('./data/hybrid.db')
-if st.button("Login"):
+with st.sidebar:
+    user_name = st.text_input("Username")
+    logbutton = st.button("Login")
+
+
+if logbutton:
     relate = "SELECT username,leftusername,rightusername FROM users WHERE (((users.username)=" + "'" + user_name + "'" + "));"
     relatefile = pd.read_sql(relate,conn,)
     current_user = list(relatefile['username'])
-    level0,level0c,level0r = st.columns(3)
+    level2l, evel1l,level0c,level1r ,level2r = st.columns(5)
     with level0c:
-        st.image("./pix/user.png",caption=  current_user,width=300,)
-
-    level1l,level1c,level1r=st.columns(3)
+        st.image("./pix/user.png",caption=  current_user,width=150,)
+    
+    level2l,level1l, level0c,level1r,level2r = st.columns(5)
     with level1l:
-        st.image("./pix/user.png",caption = list(relatefile['leftusername']),width=300)
+        st.image("./pix/user.png",caption = list(relatefile['leftusername']),width=150)
     with level1r:
-        st.image("./pix/user.png",caption = list(relatefile['rightusername']),width=300)
+        st.image("./pix/user.png",caption = list(relatefile['rightusername']),width=150)   
+    level2l,level2lc,level1l, level0c,level1r,level2rc, level2r = st.columns(7)
+    with level2l:
+        st.image("./pix/user.png", use_container_width = True )
+    with level2r:
+        st.image("./pix/user.png", use_container_width = True)
+    with level1r:
+        st.image("./pix/user.png", use_container_width = True )
+    with level1l:
+        st.image("./pix/user.png", use_container_width = True)
